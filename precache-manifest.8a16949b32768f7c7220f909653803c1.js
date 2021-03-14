6ddfb62c3d32b5c2e5f20fd336f65e97569bd660
chen <154724313@qq.com>
2021-03-14 19:23:58 +0800

    deploy

---
+++ precache-manifest.8a16949b32768f7c7220f909653803c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684153c5d85a5f10eeed",
+    "url": "/morney-website/css/about.b6f877b6.css"
+  },
+  {
+    "revision": "c42e296e2b20315e72ce",
+    "url": "/morney-website/css/app.476a21ba.css"
+  },
+  {
+    "revision": "676f5a4ad93d52159d85a44c45783f7b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "684153c5d85a5f10eeed",
+    "url": "/morney-website/js/about.13e87a81.js"
+  },
+  {
+    "revision": "c42e296e2b20315e72ce",
+    "url": "/morney-website/js/app.624ccfb4.js"
+  },
+  {
+    "revision": "a9cc3d281b97f13f4795",
+    "url": "/morney-website/js/chunk-vendors.cfbcc6aa.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
